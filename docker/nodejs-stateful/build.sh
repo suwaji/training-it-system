d1b81934b0751b67e69c8a7c044c2441511842fb
suwaji harsono <suwaji.harsono@mtn-fm.id>
2022-04-24 11:38:07 +0700

    update latihan kubernetes

--- docker/nodejs-stateful/build.sh
+++ docker/nodejs-stateful/build.sh
@@ -4,17 +4,18 @@ docker build -t harsonosuwaji/nodejs-stateful .
 # Push image
 docker push harsonosuwaji/nodejs-stateful
 
-# Create container
-docker container create --name nodejs-stateful harsonosuwaji/nodejs-stateful
-
-# Start container
-docker container start nodejs-stateful
-
-# See container logs
-docker container logs -f nodejs-stateful
-
-# Stop container
-docker container stop nodejs-stateful
-
-# Remove container
-docker container rm nodejs-stateful
+## Create container
+#docker container create --name nodejs-stateful harsonosuwaji/nodejs-stateful
+#
+## Start container
+#docker container start nodejs-stateful
+#
+## See container logs
+#docker container logs -f nodejs-stateful
+#
+## Stop container
+#docker container stop nodejs-stateful
+#
+## Remove container
+#docker container rm nodejs-stateful
+#
